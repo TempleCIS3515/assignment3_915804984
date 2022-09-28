79c006cb4adc5e3a38f142243a1f037c4dd98c01
NabaaAbdulhussein <111922494+NabaaAbdulhussein@users.noreply.github.com>
2022-09-28 18:23:59 -0400

    second commit. created xml under layout folder. Attached listener to spinner and made sure index 0 is not changed color since it is not a color.

--- app/src/main/java/edu/temple/assignment3/MainActivity.kt
+++ app/src/main/java/edu/temple/assignment3/MainActivity.kt
@@ -1,9 +1,12 @@
 package edu.temple.assignment3
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -26,5 +29,20 @@ class MainActivity : AppCompatActivity() {
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, colors)
         colorSpinner.adapter = arrayAdapter
 
+        //adding the listener to spinner
+        colorSpinner.onItemSelectedListener = object: AdapterView.  OnItemSelectedListener{
+            override fun onItemSelected(adapterView: AdapterView<*>, view: View?, i: Int, l: Long) {
+                if (i !=0) //first index of 0 is not a color
+                {
+                    val colorTextView = adapterView.findViewById<TextView>(R.id.color_text_view)
+                    colorTextView.setBackgroundColor(Color.WHITE)
+
+                    layout.setBackgroundColor(Color.parseColor(colors[i]))
+                }
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>) {
+            }
+        }
     }
 }
